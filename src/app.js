6499fa2dfef3bcff3af3c69b6efc90b6045ef17f
Dricosta <adrianoc55@hotmail.com>
2020-04-16 20:40:57 -0300

    challenge completed - all tests passed

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 
-// const { uuid } = require("uuidv4");
+ const { uuid } = require("uuidv4");
 
 const app = express();
 
@@ -10,24 +10,70 @@ app.use(cors());
 
 const repositories = [];
 
+function CheckRepoInArray(req, res, next) {
+  const { id } = req.params;
+
+  const validatedId = repositories.find(repo => repo.id == id);
+
+  if(!validatedId){
+      return res.status(400).json({ error: "Repository not found."});
+  }
+
+  return next();
+}
+
 app.get("/repositories", (request, response) => {
-  // TODO
+  return response.json(repositories);
 });
 
 app.post("/repositories", (request, response) => {
-  // TODO
+  const { title, url, techs } = request.body;
+
+  const repository = { id: uuid(), title, url, techs, likes: 0 }
+
+  repositories.push(repository);
+
+  return response.status(200).json(repository);
 });
 
-app.put("/repositories/:id", (request, response) => {
-  // TODO
+app.put("/repositories/:id", CheckRepoInArray, (request, response) => {
+  const { title, url, techs } = request.body;
+  const { id } = request.params;
+
+  const repoIndex = repositories.findIndex(repo => repo.id === id);
+
+  if(repoIndex < 0){
+    return response.status(400).json({ message: "Repository not found."});
+  }
+
+  const repository = {  ...repositories[repoIndex], title, url, techs };
+
+  repositories[repoIndex] = repository;
+
+  return response.status(200).json(repository);
 });
 
-app.delete("/repositories/:id", (request, response) => {
-  // TODO
+app.delete("/repositories/:id", CheckRepoInArray, (request, response) => {
+  const { id } = request.params;
+
+  if(repositories.length < 1) return res.status(400).json({ error: "There are no repositories in the array." });
+
+  const repoIndex= repositories.findIndex(repo => repo.id == id);
+
+  repositories.splice(repoIndex, 1);
+
+  return response.status(204).send();
 });
 
-app.post("/repositories/:id/like", (request, response) => {
-  // TODO
+
+app.post("/repositories/:id/like", CheckRepoInArray, (request, response) => {
+  const { id } = request.params;
+
+  const repoFilterById = repositories.find(repo => repo.id == id);
+
+  repoFilterById.likes++
+
+  return response.status(200).send(repoFilterById);
 });
 
 module.exports = app;
